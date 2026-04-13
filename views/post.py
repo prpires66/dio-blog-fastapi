0199b5b9202c77a9433b4b55e88736f0938d2315
Paulo Pires <prpires66@gmail.com>
2026-04-12 22:46:22 -0300

    fix bug timezone utc

--- views/post.py
+++ views/post.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel, AwareDatetime
+from datetime import datetime, timezone
+from pydantic import BaseModel, AwareDatetime, field_validator
+
 
 class PostOut(BaseModel):
-  id: int
-  title: str
-  content: str
-  published_at: AwareDatetime | None = None
-  
+    id: int
+    title: str
+    content: str
+    published_at: AwareDatetime | None = None
+
+    @field_validator("published_at", mode="before")
+    @classmethod
+    def ensure_timezone(cls, v: datetime | None) -> datetime | None:
+        if v is not None and v.tzinfo is None:
+            return v.replace(tzinfo=timezone.utc)
+        return v
